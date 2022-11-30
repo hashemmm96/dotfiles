9a540403dcb88df0d4849de1ada871b1604fb8d3
Hashem Hashem <hashemmm96@gmail.com>
2022-11-30 19:29:22 +0100

    chore(nvim): migrate to lua conf for nvim

---
+++ .config/nvim/lua/lang/go.lua
@@ -0,0 +1,7 @@
+vim.g.go_highlight_types = true
+vim.g.go_highlight_fields = true
+vim.g.go_highlight_functions = true
+vim.g.go_highlight_function_calls = true
+vim.g.go_highlight_extra_types = true
+vim.g.go_highlight_operators = true
+vim.g.go_doc_keywordprg_enabled = false
---
+++ .config/nvim/lua/plugins/nvim-telescope.lua
@@ -0,0 +1,18 @@
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+local actions = require('telescope.actions')
+require('telescope').setup {
+  pickers = {
+    live_grep = {
+      mappings = {
+        i = { ["<C-f>"] = actions.to_fuzzy_refine },
+      },
+    },
+  },
+}
+require('telescope').load_extension('fzf')
