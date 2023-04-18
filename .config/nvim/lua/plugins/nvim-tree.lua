d1a0e18b370b622133bee1061d446977d08c4d8c
Hashem Hashem <hashem@canedudev.com>
2023-04-18 09:45:52 +0200

    chore(nvim): update to v0.9.0

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,27 +1,20 @@
------------------------------------------------------------
--- File manager configuration file
------------------------------------------------------------
-
--- Plugin: nvim-tree
--- url: https://github.com/kyazdani42/nvim-tree.lua
-
--- Keybindings are defined in `core/keymaps.lua`:
--- https://github.com/kyazdani42/nvim-tree.lua#keybindings
 local status_ok, nvim_tree = pcall(require, 'nvim-tree')
 if not status_ok then
   return
 end
 
--- Call setup.
--- See: `:help nvim-tree` 4. SETUP
--- Each of these are documented in `:help nvim-tree.OPTION_NAME`
--- nested options are documented by accessing them with `.` (eg: `:help nvim-tree.view.mappings.list`)
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', '<C-[>', api.tree.change_root_to_parent, opts('Up'))
+end
+
 nvim_tree.setup({
-  view = {
-    mappings = {
-      list = {
-        { key = "<C-[>", action = "dir_up" },
-      },
-    },
-  },
+  on_attach = on_attach,
 })
