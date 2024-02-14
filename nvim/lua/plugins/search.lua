b2aa40d02c4cfedf2d87a6dfd9a164b84c334421
Hashem Hashem <hashem@canedudev.com>
2024-02-14 15:43:40 +0100

    feat(nvim): search hidden files with telescope

--- nvim/lua/plugins/search.lua
+++ nvim/lua/plugins/search.lua
@@ -14,13 +14,29 @@ return {
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
     local actions = require('telescope.actions')
+    local config = require('telescope.config')
+
+    -- Clone the default Telescope configuration
+    local vimgrep_arguments = { unpack(config.values.vimgrep_arguments) }
+
+    -- I want to search in hidden/dot files.
+    table.insert(vimgrep_arguments, "--hidden")
+    -- I don't want to search in the `.git` directory.
+    table.insert(vimgrep_arguments, "--glob")
+    table.insert(vimgrep_arguments, "!**/.git/*")
 
     require('telescope').setup {
+      defaults = {
+        vimgrep_arguments = vimgrep_arguments,
+      },
       pickers = {
         live_grep = {
           mappings = {
             i = { ["<C-f>"] = actions.to_fuzzy_refine },
           },
+        },
+        find_files = {
+          find_command = { "fdfind", "--type", "f", "--strip-cwd-prefix", "--hidden", "--exclude", ".git/*" },
         },
       },
     }
